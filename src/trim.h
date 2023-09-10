7affd9331089129c537c443082c0973eb4e32e14
yury-bystritskiy <118987274+yury-bystritskiy@users.noreply.github.com>
2023-09-10 11:21:52 +0300

    Add files via upload

--- src/trim.h
+++ src/trim.h
@@ -2,10 +2,10 @@
 #define TRIM
 
 // Trims the string a and returns the len of trimmed string.
-extern long trim(char * a);
+extern size_t trim(char * a);
 
 // Trims the string a of length l and returns the len of trimmed string.
-extern long triml(char * a, long l);
+extern size_t triml(char * a, size_t l);
 
 // Frees trim internal buffer.
 extern void freetrim(void);
