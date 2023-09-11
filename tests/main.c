91f727631a82f4dfd95034634bbdfebd09df251d
yury-bystritskiy <118987274+yury-bystritskiy@users.noreply.github.com>
2023-09-11 18:23:22 +0300

    Update main.c

--- tests/main.c
+++ tests/main.c
@@ -11,8 +11,12 @@ int main(int argc, char **argv)
 	// Run test.
 	if(test())
 	{
+		printf("\x1b[31m fail\n\t");
+		fail_message();
+		printf("\x1b[m\n");
 		return 1;
 	}
 
+	printf("\x1b[32m pass\x1b[m\n");
 	return 0;
 }
