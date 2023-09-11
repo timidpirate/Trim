0250cf6b870bfa2641c91a9137e55b4f132da878
yury-bystritskiy <118987274+yury-bystritskiy@users.noreply.github.com>
2023-09-11 18:36:28 +0300

    Update main.c

--- tests/main.c
+++ tests/main.c
@@ -12,13 +12,16 @@ int main(int argc, char **argv)
 	struct winsize w;
     	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 	const size_t width = w.ws_col;
-	printf("Terminal width is %ld ", width);
 	
-	// Write test name and dotted line.
-	size_t len = strlen(argv[1]);
+	// Write test name.
 	printf("Run %s ", argv[1]);
-//	for(size_t i = 0 ; i < width - sizeof("Run") - len - sizeof("pass") - 1 ; i++) putchar('.');
 
+	// Write dotted line if there are extra width.
+	size_t len = strlen(argv[1]);
+	size_t text_len = sizeof("Run") + len + sizeof("pass") + 1;
+	if(width > text_len)
+		for(size_t i = 0 ; i < width - text_len ; i++) putchar('.');
+	
 	// Run test.
 	if(test())
 	{
